64c0ab1565fe2503bd32cc9a50d1dd188c4bcb2e
Dave Snider <dave.snider@gmail.com>
2014-01-16 09:28:34 -0800

    get menu animation working again

--- public/assets/javascript/layout.js
+++ public/assets/javascript/layout.js
@@ -7,4 +7,37 @@ $( document ).ready(function() {
   $(document).on("click", ".wy-menu-vertical .current ul li a", function(event) {
     $("[data-toggle='wy-nav-shift']").removeClass("shift");
   });
+
+  // I think this can be removed and we can use the tab plugin
+  // this is a quick fix to get the fingers limber
+  // The opacity stuff should be in the class css
+  $(document).on('click', "[data-menu]", function(event) {
+
+    var target = $(this).data('menu');
+    $("[data-menu-wrap]").removeClass().addClass("move-right").css({opacity:0});
+    if (target === 'all-content') {
+      $("[data-menu-wrap='" + target + "']").removeClass().addClass("move-center").css({opacity:1});
+    } else {
+      $("[data-menu-wrap='all-content']").removeClass().addClass('move-left').css({opacity:0});
+      $("[data-menu-wrap='" + target + "']").removeClass().css({opacity:1});
+    }
+
+  });
+
+  $("[data-spy=affix]").on('affix', function (event, affix) {
+    if (!affix) {
+      $(this).css({
+        overflow: 'hidden',
+        height: Math.max.apply(null, $(this).children().map(function () { return $(this).height(); }).get())
+      });
+    } else {
+      $(this).css({
+        overflow: 'visible',
+        height: 'auto'
+      });
+    }
+  });
+
 });
+
+
