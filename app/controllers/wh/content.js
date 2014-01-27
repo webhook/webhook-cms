ab6c42973d9110e082e459b69851633099f2d4c8
Mike Horn <gpbmike@gmail.com>
2014-01-27 15:49:49 -0800

    also force on content page

--- app/controllers/wh/content.js
+++ app/controllers/wh/content.js
@@ -1,20 +1,29 @@
 export default Ember.ArrayController.extend({
   newTypeName: null,
 
+  // force a valid name
+  forceValid: function () {
+    var name = this.get('newTypeName'),
+        regex = /(\W|[A-Z])/g;
+    if (name && regex.test(name)) {
+      this.set('newTypeName', name.replace(regex, ''));
+    }
+  }.observes('newTypeName'),
+
   actions: {
     createType: function () {
       this.store.createRecord('content-type', {
         id: this.get('newTypeName'),
         name: this.get('newTypeName')
       }).save().then(function (type) {
-        this.transitionTo('form', type);
+        this.transitionToRoute('form', type);
       }.bind(this));
     },
     deleteType: function (contentType) {
       this.removeObject(contentType);
     },
     gotoEdit: function (name) {
-      this.transitionTo('form', name);
+      this.transitionToRoute('form', name);
     }
   }
 });
