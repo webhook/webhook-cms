884dc7b4d6c417515fa959ffd9c20997dfc5ecee
Mike Horn <gpbmike@gmail.com>
2014-01-27 15:36:33 -0800

    list page working

--- app/routes/wh/content/type/index.js
+++ app/routes/wh/content/type/index.js
@@ -22,48 +22,24 @@ export default Ember.Route.extend({
     }
 
     return this.store.find(contentTypeName);
-
-    // var model = Ember.A([]),
-    //     cmsFields = Ember.A([]);
-
-    // // there has to be a better way to get the type. :(
-    // var path_parts = this.modelFor('wh.content.type').get('ref.path.m'),
-    //     type = path_parts[path_parts.length - 1],
-    //     ref = new Firebase(window.ENV.firebase + "data/" + type);
-
-    // ref.on('child_added', function (snapshot) {
-    //   if (snapshot.name() === "_type") {
-    //     return;
-    //   }
-
-    //   var item = Ember.Object.create(snapshot.val());
-    //   item.set('cmsFields', Ember.A([]));
-
-    //   cmsFields.forEach(function (fieldName) {
-    //     item.get('cmsFields').pushObject(item.get(fieldName));
-    //   });
-
-    //   model.pushObject(item);
-    // });
-
-    // var typeRef = new Firebase(window.ENV.firebase + "content_types/" + type  + "/fields");
-    // typeRef.on('child_added', function (snapshot) {
-    //   if (snapshot.name() === "_type" || !snapshot.val().showInCms) {
-    //     return;
-    //   }
-    //   var fieldName = snapshot.val().name;
-
-    //   model.forEach(function (item) {
-    //     item.get('cmsFields').pushObject(item.get(fieldName));
-    //   });
-
-    //   cmsFields.pushObject(fieldName);
-    // });
-
-    // return model;
   },
   setupController: function (controller, model) {
-    controller.set('type', this.modelFor('wh.content.type'));
+
+    var type = this.modelFor('wh.content.type');
+
+    type.get('fields').then(function (fields) {
+      var cmsFieldNames = Ember.A([]);
+      fields.filterBy('showInCms').forEach(function (field) {
+        cmsFieldNames.pushObject(field.get('name'));
+      });
+      model.forEach(function (item) {
+        item.set('fields', Ember.$.map(cmsFieldNames, function (name) {
+          return item.get('data')[name];
+        }));
+      });
+    });
+
+    controller.set('contentType', type);
     this._super.apply(this, arguments);
   }
 });
