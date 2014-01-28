4ac74b122e42eaa4e18c029c4bc6b6465ada08aa
Mike Horn <gpbmike@gmail.com>
2014-01-27 17:24:48 -0800

    reset forms before you see it again. #2

--- app/routes/form.js
+++ app/routes/form.js
@@ -3,6 +3,7 @@ export default Ember.Route.extend({
     return this.store.find('content-type', params.id);
   },
   setupController: function (controller, model) {
+    controller.set('editingField', null);
     controller.set('fieldTypeGroups', this.get('store').find('field-type-group'));
     this._super.apply(this, arguments);
   }
--- app/routes/wh/content/type/new.js
+++ app/routes/wh/content/type/new.js
@@ -1,5 +1,9 @@
 export default Ember.Route.extend({
   model: function () {
     return this.modelFor('wh.content.type');
+  },
+  setupController: function (controller, model) {
+    model.get('fields').setEach('value', null);
+    this._super.apply(this, arguments);
   }
 });
