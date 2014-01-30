9fd93e090e94f3f1e3898fc8a8f5ec96e44f62f9
Mike Horn <gpbmike@gmail.com>
2014-01-29 16:48:45 -0800

    properly sync list items

--- app/controllers/wh/content/type/index.js
+++ app/controllers/wh/content/type/index.js
@@ -1,5 +1,32 @@
 export default Ember.ArrayController.extend({
   type: null,
+  cmsFieldNames: Ember.A([]),
+
+  fieldsChanged: function () {
+
+    window.console.log('fields');
+
+    // this.set('cmsFieldNames', Ember.A([]));
+
+    // this.get('type').get('fields').filterBy('showInCms').forEach(function (field) {
+    //   this.get('cmsFieldNames').pushObject(field.get('name'));
+    // });
+
+  }.observes('type.fields.@each'),
+
+  contentChanged: function () {
+
+    window.console.log('content');
+
+    this.get('content').forEach(function (item) {
+      var fieldValues = [];
+      this.get('cmsFieldNames').forEach(function (name) {
+        fieldValues.push(item.get('data')[name]);
+      });
+      item.set('fields', fieldValues);
+    }, this);
+
+  }.observes('@each'),
 
   actions: {
     deleteItem: function (item) {
--- app/routes/wh/content/type/index.js
+++ app/routes/wh/content/type/index.js
@@ -2,27 +2,19 @@ import getItemModelName from 'appkit/utils/model';
 
 export default Ember.Route.extend({
   model: function () {
-    var modelName = getItemModelName(this.modelFor('wh.content.type').get('name'));
-    return this.store.find(modelName);
+    var itemModelName = getItemModelName(this.modelFor('wh.content.type').get('name'));
+    return this.store.find(itemModelName);
   },
   setupController: function (controller, model) {
 
-    var type = this.modelFor('wh.content.type');
-
-    var cmsFieldNames = Ember.A([]);
+    var type = this.modelFor('wh.content.type'),
+        cmsFieldNames = Ember.A([]);
 
     type.get('fields').filterBy('showInCms').forEach(function (field) {
       cmsFieldNames.pushObject(field.get('name'));
     });
 
-    model.forEach(function (item) {
-      var fieldValues = [];
-      cmsFieldNames.forEach(function (name) {
-        fieldValues.push(item.get('data')[name]);
-      });
-      item.set('fields', fieldValues);
-    });
-
+    controller.set('cmsFieldNames', cmsFieldNames);
     controller.set('contentType', type);
     this._super.apply(this, arguments);
   }
