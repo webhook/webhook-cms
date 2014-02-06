ade7feaecf2492c31edd32e5918ede6793e3c87b
Dave Snider <dave.snider@gmail.com>
2014-02-06 11:06:42 -0800

    sass-contrib, remove compass stuff, various style changes

--- tasks/options/sass.js
+++ tasks/options/sass.js
@@ -1,5 +1,9 @@
 module.exports = {
   compile: {
+    options: {
+      style: 'expanded',
+      loadPath: ['vendor/bourbon/app/assets/stylesheets', 'vendor/neat/app/assets/stylesheets', 'vendor/wyrm/sass', 'vendor/font-awesome/scss']
+    },
     files: [{
       expand: true,
       cwd: 'app/styles',
