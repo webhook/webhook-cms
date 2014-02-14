f678e76751213a57bac1b381cbdddf7e843f34ed
Mike Horn <gpbmike@gmail.com>
2014-02-13 23:45:26 -0800

    update upload component to match webhook-js

--- app/components/image-upload.js
+++ app/components/image-upload.js
@@ -23,58 +23,66 @@ export default Ember.Component.extend({
         $url = this.$('.wy-form-upload-url'),
         $loading = this.$('.wy-form-upload .image-loading');
 
-    var $uploadInput = this.$('input[type=hidden]').upload({
-      uploadTrigger : this.$('.wy-form-upload button'),
-      uploadDropzone: this.$('.wy-form-upload button'),
-      uploadUrl     : window.ENV.uploadUrl,
-      uploadSite    : session.get('site.name'),
-      uploadToken   : session.get('site.token')
-    }).on({
-      'dragenter.wh.upload': function () {
-        $(this).data('upload').$triggerElement
+    var $uploadButton = this.$('.wy-form-upload-content button').upload({
+      uploadUrl  : window.ENV.uploadUrl,
+      uploadSite : session.get('site.name'),
+      uploadToken: session.get('site.token')
+    }).dropzone().on({
+      dropzonewindowenter: function () {
+        $(this)
           .removeClass('icon-image icon-desktop btn-neutral')
           .addClass('icon-arrow-down btn-success')
           .text(' Drop files here');
       },
-      'dragleave.wh.upload': resetButton,
-      'dragdrop.wh.upload': resetButton,
-      'error.wh.upload': function (event, response) {
+      dropzonewindowdrop: resetButton,
+      dropzonewindowleave: resetButton,
+      drop: function (event) {
+        $(this).upload('upload', event.originalEvent.dataTransfer.files[0]);
+        resetButton.call(this);
+      },
+      'error': function (event, response) {
         self.sendAction('notify', 'danger', 'Mike, make this error more useful. kthx.');
       },
-      'start.wh.upload': function () {
+      'start': function () {
         $container.show();
         $url.hide();
-        $(this).data('upload').$triggerElement.hide();
+        $uploadButton.hide();
         $loading.css('display', 'inline-block');
         $loading.find('span').html('Uploading <span>0%</span>');
       },
-      'thumb.wh.upload': function (event, thumb) {
+      'thumb': function (event, thumb) {
         self.set('initial', null);
         $upload.find('.wy-form-upload-image.edit').remove();
         $('<div class="wy-form-upload-image edit">')
           .prependTo($upload)
           .append(thumb);
       },
-      'progress.wh.upload': function (event, percentage) {
+      'progress': function (event, percentage) {
         if (percentage < 100) {
           $loading.find('span').html('Uploading <span>' + percentage + '%</span>');
         } else {
           $loading.find('span').text('Finishing up...');
         }
       },
-      'load.wh.upload': function (event, response) {
+      'load': function (event, response) {
         control.set('value', response.url);
         self.sendAction('notify', 'success', 'File upload complete.');
-        $(this).data('upload').$element.val(response.url);
       },
-      'done.wh.upload': function () {
+      'done': function () {
         $loading.hide();
-        $(this).data('upload').$triggerElement.show();
-      }
+        $uploadButton.show();
+      },
+      mouseenter: function () {
+        $(this)
+          .removeClass('icon-image icon-arrow-down btn-success')
+          .addClass('icon-desktop btn-neutral')
+          .text(' Select from desktop');
+      },
+      mouseleave: resetButton
     });
 
     this.$('.wy-form-upload-url .upload-url').on('click', function () {
-      $uploadInput.upload('upload', this.$('.wy-form-upload-url input').val());
+      $uploadButton.upload('upload', this.$('.wy-form-upload-url input').val());
       this.$('.wy-form-upload-url input').val('');
     }.bind(this));
 
