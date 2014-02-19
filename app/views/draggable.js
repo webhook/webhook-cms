3f8d47a695772df1c788b6c13eb6a5e62ab4982b
Mike Horn <gpbmike@gmail.com>
2014-02-18 16:59:07 -0800

    custom helper

--- app/views/draggable.js
+++ app/views/draggable.js
@@ -1,15 +1,26 @@
 export default Ember.View.extend({
 
   didInsertElement : function () {
-    this.$().on('mouseenter', function () {
-      $(this).find('dd').draggable({
-        helper: 'clone',
-        appendTo: 'body',
-        connectToSortable: Ember.$('form .ui-sortable'),
-        revert: true,
-        revertDuration: 0,
-        stack: $(this).find('dd')
-      });
+    // real dumb way of making sure the content types are there
+    this.$().on('mouseenter', this.makeDraggable.bind(this));
+  },
+
+  makeDraggable: function () {
+    this.$('dd').draggable({
+      helper: function (event) {
+
+        var helper = $('<div class="wh-form-control-clone">'),
+            icon = $('<span>').appendTo(helper);
+
+        icon.addClass($(this).find('a').attr('class')).text($(this).text());
+
+        return helper;
+
+      },
+      appendTo: 'body',
+      connectToSortable: Ember.$('form .ui-sortable'),
+      revert: true,
+      revertDuration: 0
     });
   }
 
