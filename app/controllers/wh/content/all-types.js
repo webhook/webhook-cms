5531b880e523330ef5e3901ad23a68eec80d3c60
Mike Horn <gpbmike@gmail.com>
2014-02-20 14:13:27 -0800

    change controller to match route, fix deleting content types

--- app/controllers/wh/content/all-types.js
+++ app/controllers/wh/content/all-types.js
@@ -2,6 +2,7 @@ export default Ember.ArrayController.extend({
   actions: {
     deleteType: function (contentType) {
       this.removeObject(contentType);
+      contentType.destroyRecord();
     },
     gotoEdit: function (name) {
       this.transitionToRoute('form', name);
