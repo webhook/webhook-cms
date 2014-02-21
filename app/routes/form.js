00e18065d7344d51c5d7fa6a90b7d1efd5df6968
Mike Horn <gpbmike@gmail.com>
2014-02-21 13:01:11 -0800

    do not keep isEditing state

--- app/routes/form.js
+++ app/routes/form.js
@@ -4,6 +4,7 @@ export default Ember.Route.extend({
   },
   setupController: function (controller, model) {
     controller.set('editingControl', null);
+    controller.set('isEditing', false);
     controller.set('controlTypeGroups', this.get('store').find('control-type-group'));
     this._super.apply(this, arguments);
   }
