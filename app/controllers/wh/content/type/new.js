251108618da759e68332941b9692e6a110de53f1
Mike Horn <gpbmike@gmail.com>
2014-02-24 17:09:24 -0800

    duplicate view method on new controller as well

--- app/controllers/wh/content/type/new.js
+++ app/controllers/wh/content/type/new.js
@@ -8,31 +8,49 @@ export default Ember.ObjectController.extend({
     this.get('model.controls').forEach(validateControl);
   }.observes('model.controls.@each.value'),
 
-  actions: {
-    createItem: function () {
+  saveItem: function () {
 
-      if (!this.get('model.controls').isEvery('isValid')) {
-        window.alert('Fix your problems.');
-        return;
+    if (!this.get('model.controls').isEvery('isValid')) {
+      window.alert('Fix your problems.');
+      return;
+    }
+
+    var data = {};
+
+    // gather and clean data for storage
+    this.get('model.controls').filterBy('value').forEach(function (control) {
+      var value = control.get('value');
+
+      if (control.get('controlType.valueType') === 'object') {
+        Ember.$.each(value, function (key, childValue) {
+          if (!childValue) {
+            delete value[key];
+          }
+        });
       }
 
-      var data = {};
+      data[control.get('name')] = value;
+    });
 
-      this.get('model.controls').filterBy('value').forEach(function (control) {
-        data[control.get('name')] = control.get('value');
+    // checkboxes are special
+    this.get('model.controls').filterBy('controlType.widget', 'checkbox').forEach(function (control) {
+      data[control.get('name')] = [];
+      control.get('meta.data.options').forEach(function (option) {
+        data[control.get('name')].push(option);
       });
+    });
 
-      var modelName = getItemModelName(this.get('model'));
+    var modelName = getItemModelName(this.get('model'));
 
-      this.store.createRecord(modelName, {
-        data: data
-      }).save().then(function () {
-        window.ENV.sendBuildSignal();
+    this.store.createRecord(modelName, {
+      data: data
+    }).save().then(function () {
+      window.ENV.sendBuildSignal();
 
-        this.send('notify', 'success', 'Item created!');
-        this.transitionToRoute('wh.content.type', this.get('model'));
-      }.bind(this));
+      this.send('notify', 'success', 'Item created!');
+      this.transitionToRoute('wh.content.type', this.get('model'));
+    }.bind(this));
 
-    }
   }
+
 });
