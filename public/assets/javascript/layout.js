4c290f80388fdee1f2515b024683b7ae6990a1b1
Dave Snider <dave.snider@gmail.com>
2014-02-27 09:27:47 -0800

    hmm, need mike maybe

--- public/assets/javascript/layout.js
+++ public/assets/javascript/layout.js
@@ -1,4 +1,8 @@
 $( document ).ready(function() {
+  // Highlight anything in pre tags, this comes from Wyrm
+  $('pre').addClass('highlight');
+  $('pre.highlight').each(function(i, e) {hljs.highlightBlock(e)});
+
   // Shift nav in mobile when clicking the menu.
   $(document).on('click', "[data-toggle='wy-nav-top']", function(event) {
     $("[data-toggle='wy-nav-shift']").toggleClass("shift");
