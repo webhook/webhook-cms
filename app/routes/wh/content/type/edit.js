c3bfd0fdf02776febd17e9818f1b81dacf3087cc
Mike Horn <gpbmike@gmail.com>
2014-02-27 14:35:39 -0800

    dateTime controls with defaults and fixes in place.

--- app/routes/wh/content/type/edit.js
+++ app/routes/wh/content/type/edit.js
@@ -2,13 +2,22 @@ import getItemModelName from 'appkit/utils/model';
 
 export default Ember.Route.extend({
   beforeModel: function () {
-    // need to make sure all the content types are in the store
-    // basically a hack
-    return this.store.find('control-type');
+    // return this.store.find('control-type');
+    return Ember.RSVP.Promise.all([
+      // need to make sure all the content types are in the store
+      // basically a hack
+      this.store.find('control-type'),
+      // make sure `create_date`, `last_updated` and `publish_date` controls exist
+      this.fixControlType(this.modelFor('wh.content.type'))
+    ]);
   },
   model: function (params) {
     var modelName = getItemModelName(this.modelFor('wh.content.type'));
     return this.store.find(modelName, params.item_id);
+  },
+  afterModel: function (model) {
+    // Make sure item has `create_date` value
+    return this.fixItem(model);
   },
   setupController: function (controller, model) {
 
@@ -18,9 +27,6 @@ export default Ember.Route.extend({
 
     var data = model.get('data'),
         type = this.modelFor('wh.content.type');
-
-    controller.set('publishDate', data.publishDate ? moment(data.publishDate).format('YYYY-MM-DDTHH:mm') : null);
-    controller.set('isDraft', data.isDraft || !controller.get('publishDate'));
 
     type.get('controls').forEach(function (control) {
 
@@ -37,13 +43,7 @@ export default Ember.Route.extend({
 
       // remove offset so datetime input can display
       if (value && control.get('controlType.widget') === 'datetime') {
-
         value = moment(value).format('YYYY-MM-DDTHH:mm');
-
-        // Set `lastUpdated` and `createDate` on controller for Dave.
-        if (['last_updated', 'create_date'].indexOf(control.get('name')) >= 0) {
-          controller.set(control.get('name').camelize(), value);
-        }
       }
 
       if (!value && control.get('controlType.valueType') === 'object') {
@@ -53,7 +53,61 @@ export default Ember.Route.extend({
       control.set('value', value);
     });
 
+    controller.set('publishDate', type.get('controls').findBy('name', 'publish_date').get('value'));
+    controller.set('isDraft', data.isDraft || !controller.get('publishDate'));
+
     controller.set('type', type);
     this._super.apply(this, arguments);
+  },
+
+  fixControlType: function (contentType) {
+
+    return this.store.find('control-type', 'datetime').then(function (controlType) {
+
+      var datetimeDefaults = {
+        controlType: controlType,
+        locked     : true,
+        showInCms  : true,
+        required   : true,
+        hidden     : true
+      };
+
+      var controls = contentType.get('controls');
+
+      var addControl = function (data) {
+        controls.pushObject(this.store.createRecord('control', Ember.$.extend({}, datetimeDefaults, data)));
+      };
+
+      if (!controls.isAny('name', 'create_date')) {
+        addControl({
+          name : 'create_date',
+          label: 'Create Date'
+        });
+      }
+
+      if (!controls.isAny('name', 'last_updated')) {
+        addControl({
+          name : 'last_updated',
+          label: 'Last Updated'
+        });
+      }
+
+      if (!controls.isAny('name', 'publish_date')) {
+        addControl({
+          name : 'publish_date',
+          label: 'Publish Date',
+          required: false
+        });
+      }
+
+      contentType.save();
+
+    }.bind(this));
+
+  },
+  fixItem: function (item) {
+    if (!item.get('data').create_date) {
+      item.get('data').create_date = moment().format('YYYY-MM-DDTHH:mm');
+    }
   }
 });
