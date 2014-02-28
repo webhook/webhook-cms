b01a2efa25722183ccc27c71eb712e89440feaa7
Mike Horn <gpbmike@gmail.com>
2014-02-28 15:37:19 -0800

    markdown editor

---
+++ app/views/markdown-editor.js
@@ -0,0 +1,28 @@
+export default Ember.View.extend({
+  classNames: ['wh-markdown-editor'],
+  classNameBindings: ['whMarkdownEditorFullscreen'],
+
+  wyMarkdownEditorFullscreen: false,
+
+  didInsertElement: function () {
+    this.$('.fullscreen-toggle').on('click', this.toggleFullscreen.bind(this));
+
+  },
+
+  toggleFullscreen: function () {
+    this.toggleProperty('whMarkdownEditorFullscreen');
+    this.$('.fullscreen-toggle').toggleClass('icon-fullscreen icon-resize-small');
+
+    if (this.get('whMarkdownEditorFullscreen')) {
+      this.syncPreview();
+      this.$('textarea').on('keyup', this.syncPreview.bind(this));
+    } else {
+      this.$('textarea').off('keyup', this.syncPreview.bind(this));
+    }
+  },
+
+  syncPreview: function () {
+    this.$('.wh-markdown-preview').html(markdown.toHTML(this.$('textarea').val()));
+  }
+
+});
