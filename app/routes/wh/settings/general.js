ab585e2fb3b360ab4a9f96699acb0e87115f3384
Mike Horn <gpbmike@gmail.com>
2014-03-13 20:39:36 -0700

    create general settings if they do not exist

--- app/routes/wh/settings/general.js
+++ app/routes/wh/settings/general.js
@@ -1,9 +1,11 @@
 export default Ember.Route.extend({
   model: function () {
-    return this.store.find('settings', 'site');
-  },
+    return this.store.find('settings', 'general').then(null, function () {
 
-  afterModel: function (model) {
-    window.console.log(model.get('siteName'));
+      var settings = this.store.push('settings', { id: 'general' });
+
+      return Ember.RSVP.resolve(settings);
+
+    }.bind(this));
   }
 });
