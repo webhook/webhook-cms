2cd1b022933467285125c0375a3d596d79d7d105
Ian Kelly <ltsquigs@gmail.com>
2014-03-18 15:23:52 -0700

    basic domain name stuff on this side

---
+++ app/controllers/wh/settings/domain.js
@@ -0,0 +1,28 @@
+export default Ember.ObjectController.extend({
+  domain : "",
+
+  isSending: false,
+  success  : false,
+  errors   : Ember.A([]),
+
+
+  actions: {
+    updateDns: function() {
+      this.set('isSending', true);
+
+      var domain = this.get('domain');
+      var siteName = this.get('buildEnvironment').siteName;
+
+      window.ENV.firebaseRoot.child("management/sites/" + siteName + "/dns").set(domain, function() {
+        var commandData = {
+          dnsname: domain,
+        };
+
+        window.ENV.firebaseRoot.child("management/commands/dns/" + siteName).set(commandData, function() {
+          this.set('success', true);
+          this.set('isSending', false);
+        }.bind(this));
+      }.bind(this));
+    }
+  }
+});
