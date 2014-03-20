58e97792f5fb7a33947fedb0a310c3520985b9e1
Mike Horn <gpbmike@gmail.com>
2014-03-20 15:50:09 -0700

    get all of your data minus presence

--- app/controllers/wh/settings/data.js
+++ app/controllers/wh/settings/data.js
@@ -1,8 +1,15 @@
 export default Ember.Controller.extend({
   actions: {
     download: function () {
-      window.ENV.firebase.child('data').once('value', function (snapshot) {
-        var blob = new window.Blob([JSON.stringify(snapshot.val(), null, 2)], {type: "text/plain;charset=utf-8"});
+      window.ENV.firebase.once('value', function (snapshot) {
+        var data = snapshot.val();
+
+        // we don't need presence data.
+        if (data.presence) {
+          delete data.presence;
+        }
+
+        var blob = new window.Blob([JSON.stringify(data, null, 2)], {type: "text/plain;charset=utf-8"});
         window.saveAs(blob, moment().format() + '.json');
       });
     }
