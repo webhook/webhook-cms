ba9ef176c86fa1e3e41e2446c96d7ba2fc581123
Mike Horn <gpbmike@gmail.com>
2014-03-24 14:35:16 -0700

    get tests working

---
+++ tests/unit/components/template-less-component-test.js
@@ -0,0 +1,9 @@
+import { test, moduleForComponent } from 'ember-qunit';
+
+
+moduleForComponent('template-less');
+
+test("template", function(){
+  var component = this.subject();
+  ok(this.$());
+});
