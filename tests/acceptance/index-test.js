8a2c6537fbc42f22d2a915f9206ecf8d31aee361
Mike Horn <gpbmike@gmail.com>
2014-03-26 11:44:17 -0700

    only unit tests for now

--- tests/acceptance/index-test.js
+++ tests/acceptance/index-test.js
@@ -1,22 +1,22 @@
-var App;
+// var App;
 
-module('Acceptances - Index', {
-  setup: function(){
-    App = startApp();
-  },
-  teardown: function() {
-    Ember.Logger.info('DESTROY APP');
-    Ember.run(App, 'destroy');
-  }
-});
+// module('Acceptances - Index', {
+//   setup: function(){
+//     App = startApp();
+//   },
+//   teardown: function() {
+//     Ember.Logger.info('DESTROY APP');
+//     Ember.run(App, 'destroy');
+//   }
+// });
 
-asyncTest('index renders', function(){
-  expect(1);
+// asyncTest('index renders', function(){
+//   expect(1);
 
-  visit('/').then(function(){
-    Ember.Logger.info('look for login');
-    var loginForm = find('.wh-login');
-    equal(loginForm.length, 1);
-    start();
-  });
-});
+//   visit('/').then(function(){
+//     Ember.Logger.info('look for login');
+//     var loginForm = find('.wh-login');
+//     equal(loginForm.length, 1);
+//     start();
+//   });
+// });
