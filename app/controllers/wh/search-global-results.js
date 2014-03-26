9f14bf61a81ff26e098dfd80b926ff24f4a98061
Mike Horn <gpbmike@gmail.com>
2014-03-26 15:50:47 -0700

    clean up the search stuff a bit

--- app/controllers/wh/search-global-results.js
+++ app/controllers/wh/search-global-results.js
@@ -1,27 +1,19 @@
 export default Ember.ArrayController.extend({
   needs: "wh",
   searchQuery: Ember.computed.alias("controllers.wh.searchQuery"),
-  isLoading: false,
-  isEmpty: false,
-
-  results: [],
+  debouncedQuery: '',
 
   searchQueryObserver: Ember.debouncedObserver(function() {
 
-    if(!this.get('searchQuery')) {
-      this.set('isLoading', false);
-      this.set('isEmpty', true);
-
-    } else {
-      this.set('isEmpty', false);
+    if (this.get('searchQuery')) {
       this.set('isLoading', true);
-
+      this.set('debouncedQuery', this.get('searchQuery'));
       window.ENV.search(this.get('searchQuery'), 1, function(err, data) {
-        this.set('results', data);
+        this.set('model', data);
         this.set('isLoading', false);
       }.bind(this));
     }
 
-  }, 'searchQuery', 1000).on('init'),
+  }, 'searchQuery', 500).on('init'),
 
 });
--- app/routes/wh/search-global-results.js
+++ app/routes/wh/search-global-results.js
@@ -1,10 +1,20 @@
 export default Ember.Route.extend({
 
-  setupController: function (controller) {
-    controller.setProperties({
-      isLoading: true
-    });
+  model: function () {
+    var searchQuery = this.controllerFor('wh').get('searchQuery');
+    return new Ember.RSVP.Promise(function (resolve, reject) {
+      if (searchQuery) {
+        window.ENV.search(searchQuery, 1, function(err, data) {
+          if (err) {
+            Ember.run(null, reject, err);
+          } else {
+            Ember.run(null, resolve, data);
+          }
+        });
+      } else {
+        Ember.run(null, resolve, Ember.A([]));
+      }
+    }.bind(this));
+  }
 
-    this._super.apply(this, arguments);
-  },
 });
