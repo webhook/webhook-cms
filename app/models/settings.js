cfbaa7223f7ef9afdfc11219f18771d535d32eed
Mike Horn <gpbmike@gmail.com>
2014-04-09 12:38:26 -0700

    spacing people

--- app/models/settings.js
+++ app/models/settings.js
@@ -1,9 +1,9 @@
 export default DS.Model.extend({
-  siteName: DS.attr('string'),
-  siteUrl: DS.attr('string'),
+  siteName       : DS.attr('string'),
+  siteUrl        : DS.attr('string'),
   siteDescription: DS.attr('string'),
-  siteKeywords:    DS.attr('string'),
-  analyticsId:    DS.attr('string'),
-  siteTwitter:    DS.attr('string'),
-  siteFacebook:    DS.attr('string')
+  siteKeywords   : DS.attr('string'),
+  analyticsId    : DS.attr('string'),
+  siteTwitter    : DS.attr('string'),
+  siteFacebook   : DS.attr('string')
 });
