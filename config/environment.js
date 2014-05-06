4f7e7ee4d287f1de8f784e1eca1fc9f5ab2c89b6
Dave Snider <dave.snider@gmail.com>
2014-05-06 11:23:33 -0700

    github links

--- config/environment.js
+++ config/environment.js
@@ -19,7 +19,7 @@ window.ENV = {
       image: 'https://raw.githubusercontent.com/webhook/webhook-theme-podcast/master/static/images/theme-screenshot.png',
       url:   'https://github.com/webhook/webhook-theme-podcast/archive/master.zip',
       demo:  'http://www.dadstrength.tv',
-      code:  'http://www.dadstrength.tv',
+      code:  'https://github.com/webhook/webhook-theme-podcast/archive/master.zip',
       description: 'Podcast theme with simple blog. Comes with a persistant audio player and iTunes formatted RSS.'
     },
     {
@@ -27,7 +27,7 @@ window.ENV = {
       image: 'https://raw.github.com/snide/webhook-theme-bootstrap-blog/master/theme_screenshot.png',
       url:   'https://github.com/snide/webhook-theme-bootstrap-blog/archive/master.zip',
       demo:  'http://daveblog.webhook.org',
-      code:  'http://www.dadstrength.tv',
+      code:  'https://github.com/snide/webhook-theme-bootstrap-blog',
       description: 'Installs a very basic blog using Bootstrap for some minimal styling. Also loads in some starter forms.'
     },
     {
