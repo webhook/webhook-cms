47f2f17e3ce76ab495dd0db0faafacf137ba548f
Ian Kelly <ltsquigs@gmail.com>
2014-05-14 11:16:04 -0700

    freaking lint

--- app/app.js
+++ app/app.js
@@ -388,7 +388,7 @@ Ember.Application.initializer({
 
     function uniqueId() {
       return Date.now() + 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+        var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
         return v.toString(16);
       }); 
     }
--- app/controllers/wh/settings/team.js
+++ app/controllers/wh/settings/team.js
@@ -16,7 +16,7 @@ export default Ember.ObjectController.extend({
 
     function uniqueId() {
       return Date.now() + 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+        var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
         return v.toString(16);
       }); 
     }
