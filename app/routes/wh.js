793176e5a3b9fadcfaadb0e515bdac624575f5f9
Mike Horn <gpbmike@gmail.com>
2014-05-15 11:02:35 -0700

    we have to do this the normal way so it updates live

--- app/routes/wh.js
+++ app/routes/wh.js
@@ -3,9 +3,9 @@ export default Ember.Route.extend({
 
     // "A record can also enter the empty state if the adapter is unable to locate the record."
     // We do not want to show those records so filter them out.
-    return this.store.find('content-type').then(function (contentTypes) {
-      return contentTypes.filterBy('currentState.isEmpty', false);
-    });
+    // Unfortunately our adapter doesn't support queries so we can't do live filtering.
+    // todo: live filtering.
+    return this.store.find('content-type');
 
   }
 });
