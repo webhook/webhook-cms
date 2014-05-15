7ba19dd95675847354eb0e53760654e04581cea1
Dave Snider <dave.snider@gmail.com>
2014-05-14 20:08:24 -0700

    video theme

--- config/environment.js
+++ config/environment.js
@@ -15,7 +15,7 @@ window.ENV = {
 
   themes: [
     {
-      name:  'Podcast + Blog',
+      name:  'Podcast & blog',
       image: 'https://raw.githubusercontent.com/webhook/webhook-theme-podcast/master/static/images/theme-screenshot.png',
       url:   'https://github.com/webhook/webhook-theme-podcast/archive/master.zip',
       demo:  'http://www.dadstrength.tv',
@@ -23,7 +23,15 @@ window.ENV = {
       description: 'Podcast theme with simple blog. Comes with a persistant audio player and iTunes formatted RSS.'
     },
     {
-      name:  'Bootstrap Blog',
+      name:  'Video, livestreams & blog',
+      image: 'https://raw.githubusercontent.com/webhook/webhook-theme-streamer/master/static/images/theme-screenshot.png',
+      url:   'https://github.com/webhook/webhook-theme-streamer/archive/master.zip',
+      demo:  'http://webhook-theme-streamer.webhook.org',
+      code:  'https://github.com/webhook/webhook-theme-streamer',
+      description: 'Theme for video producers who manage multiple shows across YouTube, Vimeo or Twitch livestreamers.'
+    },
+    {
+      name:  'Bootstrap blog',
       image: 'https://raw.github.com/snide/webhook-theme-bootstrap-blog/master/theme_screenshot.png',
       url:   'https://github.com/snide/webhook-theme-bootstrap-blog/archive/master.zip',
       demo:  'http://daveblog.webhook.org',
@@ -31,12 +39,12 @@ window.ENV = {
       description: 'Installs a very basic blog using Bootstrap for some minimal styling. Also loads in some starter forms.'
     },
     {
-      name:  'Wyrm Sass',
+      name:  'Wyrm sass',
       image: 'https://raw.githubusercontent.com/webhook/webhook-theme-wyrm/master/static/images/theme-screenshot.png',
       url:   'https://github.com/webhook/webhook-theme-wyrm/archive/master.zip',
       demo:  'http://www.wyrmsass.org',
       code:  'https://github.com/webhook/webhook-theme-wyrm',
-      description: 'Installs boilerplate templates for the Wyrm Sass framework. Requires bower to use.'
+      description: 'Installs boilerplate templates for the Wyrm Sass framework. Requires bower.js and Sass to use. Check github for installation instructions.'
     }
   ],
   controlTypeGroups: [
