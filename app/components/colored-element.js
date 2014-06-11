6cde7c199973aacba07fa38e11de00a5b9bf5feb
Dave Snider <dave.snider@gmail.com>
2014-06-11 11:07:44 -0700

    color widget

--- app/components/colored-element.js
+++ app/components/colored-element.js
@@ -1,6 +1,6 @@
 export default Ember.Component.extend({
   tagName: 'span',
-  classNames: 'colored-element',
+  classNames: 'wh-color-value',
   attributeBindings: ['customColor:style'],
   customColor: function () {
     return 'background-color:' + this.get('color');
