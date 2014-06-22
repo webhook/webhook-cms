916d6dbeb52e6fa895e48d65e5fd66a9818b74be
Mike Horn <gpbmike@gmail.com>
2014-06-22 14:42:45 -0700

    more flexible star rating.

--- app/views/star-rating.js
+++ app/views/star-rating.js
@@ -1,38 +1,72 @@
 export default Ember.View.extend({
   tagName: 'span',
-  classNames: ['wy-star'],
-  classNameBindings: ['stars'],
 
-  stars: function () {
-    var classStr = 'wy-star-',
-        value = this.get('value'),
-        wholes = Math.floor(value),
-        half = value > wholes;
+  emptyStar: "&#xf006;",
+  fullStar: "&#xf005;",
 
-    return classStr + wholes + (half ? 'dot5' : '');
-  }.property('value'),
+  letterSpacing: 3,
 
   didInsertElement: function () {
 
-    var self = this,
-        options = this.get('control.meta.data'),
-        $element = this.$();
+    var rating = this;
+    var options = this.get('control.meta.data');
+    this.set('options', Ember.Object.create({
+      min: parseInt(options.min, 10),
+      max: parseInt(options.max, 10),
+      step: parseFloat(options.step)
+    }));
 
-    self.set('value', self.getWithDefault('control.value', 0));
-
-    $element.on('mousemove', function (event) {
-      var number = event.offsetX / $element.width() * options.max;
-      self.set('value', Math.round(number * (1 / options.step)) / (1 / options.step));
+    this.$().css({
+      display: 'inline-block',
+      maxWidth: '100%',
+      letterSpacing: this.get('letterSpacing'),
+      color: '#b3b3b3',
+      fontFamily: 'hook',
+      verticalAlign: 'top',
+      position: 'relative',
+      cursor: 'pointer'
     });
 
-    $element.on('mouseout', function () {
-      self.set('value', self.getWithDefault('control.value', 0));
+    rating.$().html(new Array(this.get('options.max') + 1).join(rating.get('emptyStar')));
+
+    rating.set('stars', this.$('<span>').css({
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      overflow: 'hidden'
+    }).html(new Array(this.get('options.max') + 1).join(rating.get('fullStar'))));
+
+    rating.get('stars').appendTo(rating.$());
+
+    rating.$().on('mousemove', function (event) {
+      var number = event.offsetX / rating.$().width() * rating.get('options.max');
+      var inverseStep = 1 / rating.get('options.step');
+      var newValue = Math.ceil(number * inverseStep) / inverseStep;
+
+      rating.set('value', newValue < rating.get('options.min') ? rating.get('options.min') : newValue);
     });
 
-    $element.on('click', function () {
-      self.set('control.value', self.get('value'));
+    rating.$().on('mouseout', function (event) {
+      rating.set('value', rating.getWithDefault('control.value', rating.get('options.min')));
     });
 
-  }
+    rating.$().on('click', function () {
+      rating.set('control.value', rating.get('value'));
+    });
+
+    rating.set('value', rating.getWithDefault('control.value', rating.get('options.min')));
+
+  },
+
+  updateStars: function () {
+
+    var value = this.get('value'),
+        wholes = Math.floor(value),
+        starWidth = this.$().width() / this.get('options.max'),
+        remainder = ((value - wholes) * starWidth) - this.get('letterSpacing') / 2;
+
+    this.get('stars').width((starWidth * wholes) + (remainder));
+
+  }.observes('value')
 
 });
