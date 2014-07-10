b361b9dcc6a0785234cb2da277d6e6339e768a02
Mike Horn <gpbmike@gmail.com>
2014-07-10 15:53:10 -0700

    strip html before truncating. use for wysiwyg as well

--- app/helpers/truncate-string.js
+++ app/helpers/truncate-string.js
@@ -1,14 +1,14 @@
 export default Ember.Handlebars.makeBoundHelper(function(str,len) {
     if (!str || !len) { return str; }
 
+    // strip the html
+    str = Ember.$('<span>').html(str).text();
+
     if (str.length > len && str.length > 0) {
         var new_str = str + " ";
         new_str = str.substr(0, len);
         new_str = str.substr(0, new_str.lastIndexOf(" "));
         new_str = (new_str.length > 0) ? new_str : str.substr(0, len);
-
-        // strip the html
-        new_str = Ember.$('<span>').html(new_str).text();
 
         return new Ember.Handlebars.SafeString(new_str + '...');
     }
