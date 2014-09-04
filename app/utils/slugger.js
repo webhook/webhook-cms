fbd0685e4a3b9b6e21d12a633ca071fcea6d9470
Ian Kelly <ltsquigs@gmail.com>
2014-09-04 12:15:56 -0700

    lil slugger

---
+++ app/utils/slugger.js
@@ -0,0 +1,50 @@
+export default function slugger (item, customUrls) {
+  var tmpSlug = '';
+  tmpSlug = slug(item.name).toLowerCase();
+
+  if(customUrls && customUrls.individualUrl) {
+    tmpSlug = parseCustomUrl(customUrls.individualUrl, item) + '/' + tmpSlug;
+  } 
+
+  if(customUrls && customUrls.listUrl) {
+    tmpSlug = customUrls.listUrl + '/' + tmpSlug;
+  } else {
+    tmpSlug = type + '/' + tmpSlug;
+  }
+
+  return tmpSlug
+}
+
+function parseCustomUrl = function(url, object) {
+  var publishDate = object.publish_date ? object.publish_date : object;
+
+   publishDate = moment(publishDate);
+
+  function replacer(match, timeIdent, offset, string){
+    if(timeIdent === 'Y') {
+      return publishDate.format('YYYY').toLowerCase();
+    } else if (timeIdent === 'y') {
+      return publishDate.format('YY').toLowerCase();
+    } else if (timeIdent === 'm') {
+      return publishDate.format('MM').toLowerCase();
+    } else if (timeIdent === 'n') {
+      return publishDate.format('M').toLowerCase();
+    } else if (timeIdent === 'F') {
+      return publishDate.format('MMMM').toLowerCase();
+    } else if (timeIdent === 'M') {
+      return publishDate.format('MMM').toLowerCase();
+    } else if (timeIdent === 'd') {
+      return publishDate.format('DD').toLowerCase();
+    } else if (timeIdent === 'j') {
+      return publishDate.format('D').toLowerCase();
+    } else if (timeIdent === 'T') {
+      return object._type.toLowerCase();
+    } else {
+      return match;
+    }
+  }
+
+  url = url.replace(/#(\w)/g, replacer);
+
+  return url;
+}
