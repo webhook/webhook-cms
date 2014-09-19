f8e960e5f77867b0283fc571f5457fa47725275f
Mike Horn <gpbmike@gmail.com>
2014-09-19 11:29:29 -0700

    widgets

--- app/controllers/form.js
+++ app/controllers/form.js
@@ -22,28 +22,6 @@ export default Ember.ObjectController.extend(Ember.Evented, {
   isEditingTypeId: false,
   newTypeIdErrors: Ember.A([]),
 
-  // translatedControlTypeGroups: function () {
-  //
-  //   var translatedGroups = Ember.A([]);
-  //
-  //   this.get('controlTypeGroups').forEach(function (group) {
-  //     var translatedGroup = Ember.Object.createWithMixins(Ember.I18n.TranslateableProperties, {
-  //       nameTranslation: 'form.group.' + group.get('name').toLowerCase()
-  //     });
-  //
-  //     // window.console.log(group);
-  //
-  //     group.get('controlTypes').then(function (controlTypes) {
-  //       window.console.log(controlTypes);
-  //     });
-  //
-  //     translatedGroups.push(translatedGroup);
-  //   });
-  //
-  //   return translatedGroups;
-  //
-  // }.property(),
-
   isValidTypeId: function () {
 
     var valid = true;
