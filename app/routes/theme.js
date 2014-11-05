072360a7650682989c2c37e5f00a7bd8a3712586
Mike Horn <gpbmike@gmail.com>
2014-11-05 11:11:06 -0800

    warn user when they have no content types live. protect start, theme, form routes. fixes #142

--- app/routes/theme.js
+++ app/routes/theme.js
@@ -1,4 +1,10 @@
 export default Ember.Route.extend({
+  beforeModel: function () {
+    if (!this.get('buildEnvironment.local')) {
+      this.transitionTo('wh');
+    }
+    this._super.apply(this, arguments);
+  },
   setupController: function (controller) {
     controller.setProperties({
       themes: window.ENV.themes,
