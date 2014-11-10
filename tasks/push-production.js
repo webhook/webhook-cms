cd964c8947fc1eaf92174f505f11dbb3b723ec59
Mike Horn <gpbmike@gmail.com>
2014-11-10 14:31:51 -0800

    ordering tag for testing

--- tasks/push-production.js
+++ tasks/push-production.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
 
 
   var productionBucket = 'cms.webhook.com';
-  var productionVersion = 'v2';
+  var productionVersion = 'ordering';
   var distDir = 'dist/assets/';
 
   grunt.registerTask('push-prod', function() {
