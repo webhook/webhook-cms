f877745f84d29998aab1b3a5431e90a43eda1fc4
Mike Horn <gpbmike@gmail.com>
2014-11-10 13:47:17 -0800

    update emberfire for firebase 2.x support. fix settings.

--- app/controllers/wh/settings/general.js
+++ app/controllers/wh/settings/general.js
@@ -2,6 +2,7 @@ export default Ember.ObjectController.extend({
   actions: {
     saveSettings: function () {
       var controller = this;
+      window.console.log(controller.get('model'));
       controller.get('model').save().then(function () {
         controller.send('notify', 'success', 'Settings saved!');
         controller.send('buildSignal');
