d82c13f4c833e27752c79861c81479fd1525fbd1
Ian Kelly <ltsquigs@gmail.com>
2014-12-03 16:14:16 -0800

    warning translatable

--- app/controllers/wh/settings/data.js
+++ app/controllers/wh/settings/data.js
@@ -176,7 +176,7 @@ export default Ember.Controller.extend({
     refreshApi: function() {
       var newKey = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {var r = Math.random()*16|0,v=c==='x'?r:r&0x3|0x8;return v.toString(16);});
 
-      var allow = confirm('Are you sure you want to reset your API Key?');
+      var allow = confirm(Ember.I18n.translations['form']['widget']['wh.settings.data.api.warning']);
 
       if(!allow) {
         return;
