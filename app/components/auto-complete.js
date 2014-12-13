7f3ef68a603db580a86c99d2f0a4685fa6bc8ec4
Mike Horn <gpbmike@gmail.com>
2014-12-12 17:04:06 -0800

    check for reverse name

--- app/components/auto-complete.js
+++ app/components/auto-complete.js
@@ -30,13 +30,14 @@ export default Ember.Component.extend({
       var itemId = valueItem.split(' ')[1];
 
       var component = this;
+      var store = component.store;
 
-      this.store.find('contentType', contentTypeId).then(function (contentType) {
+      store.find('contentType', contentTypeId).then(function (contentType) {
 
         var reverseName = component.get('control.meta.reverseName');
-        var reverseIsSingle = contentType.get('controls').findBy('name', reverseName).get('meta.isSingle');
+        var reverseIsSingle = reverseName && contentType.get('controls').findBy('name', reverseName).get('meta.isSingle');
 
-        component.store.find(contentType.get('itemModelName'), itemId).then(function (model) {
+        store.find(contentType.get('itemModelName'), itemId).then(function (model) {
 
           // this feels like a hacky way to get the type and item from the edit controller
           var editController = component.get('parentView.parentView.context');
@@ -52,7 +53,7 @@ export default Ember.Component.extend({
 
           if (reverseIsSingle && reverseValue && editItemKey !== reverseValue) {
 
-            component.store.find(reverseValue.split(' ')[0], reverseValue.split(' ')[1]).then(function (reverseValueModel) {
+            store.find(reverseValue.split(' ')[0], reverseValue.split(' ')[1]).then(function (reverseValueModel) {
 
               if (window.confirm('The `%@` content type only allows one `%@` to be attached. Do you want to replace `%@` in the current relation with `%@`?'.fmt(contentType.get('name'), editType.get('name'), reverseValueModel.get('itemData.name'), editController.get('controls').findBy('name', 'name').get('value') || 'unnamed item'))) {
                 array.insertAt(insertPosition, model);
