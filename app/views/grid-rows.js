2cfd8b073a33514a9078cb5eaa37861a6066c9d0
Mike Horn <gpbmike@gmail.com>
2014-12-15 14:50:29 -0800

    use value partials. refs #160

--- app/views/grid-rows.js
+++ app/views/grid-rows.js
@@ -7,6 +7,24 @@ export default Ember.CollectionView.extend({
   itemViewClass: Ember.View.extend({
     templateName: 'grid-row',
 
+    firstControl: function () {
+      // {{#each rowControl in view.parentView.gridControl.controls}}
+
+      var control = this.get('parentView.gridControl.controls.firstObject');
+      var store = control.get('store');
+
+      // fighting with Ember to copy control
+      var controlData = control.serialize();
+      delete controlData.controls;
+      controlData.controlType = control.get('controlType');
+
+      var clone = store.createRecord('control', controlData);
+
+      clone.set('value', this.get('content').get(control.get('name')));
+
+      return clone;
+    }.property('parentView.gridControl.controls.firstObject'),
+
     isActive: function () {
       return this.get('parentView.context.activeRows').indexOf(this.get('rowIndex')) >= 0;
     }.property('parentView.context.activeRows.length', 'rowIndex'),
