5a869f1871a2469699925db6e9f9d54e8209729c
Mike Horn <gpbmike@gmail.com>
2015-01-08 22:08:17 -0800

    oops

--- app/helpers/to-markdown.js
+++ app/helpers/to-markdown.js
@@ -1,5 +1,5 @@
 export default Ember.Handlebars.makeBoundHelper(function(string) {
-  if (typeOf string === 'string') {
+  if (typeof string === 'string') {
     return new Ember.Handlebars.SafeString(marked(string));
   } else {
     return '';
