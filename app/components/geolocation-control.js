1aecd223a4fbfadfce8cc0c85f096e492b22f314
Mike Horn <gpbmike@gmail.com>
2015-01-14 10:10:46 -0800

    use modified version of leaflet relative path script

--- app/components/geolocation-control.js
+++ app/components/geolocation-control.js
@@ -8,7 +8,22 @@ export default Ember.Component.extend({
   markerInstance: null,
 
   init: function () {
-    L.Icon.Default.imagePath = '/assets/images/leaflet';
+    L.Icon.Default.imagePath = (function () {
+      var scripts = document.getElementsByTagName('script'),
+      leafletRe = /[\/^]app[\-\._]?([\w\-\._]*)\.js\??/;
+
+      var i, len, src, matches, path;
+
+      for (i = 0, len = scripts.length; i < len; i++) {
+        src = scripts[i].src;
+        matches = src.match(leafletRe);
+
+        if (matches) {
+          path = src.split(leafletRe)[0];
+          return (path ? path + '/' : '') + 'images/leaflet';
+        }
+      }
+    }());
 
     return this._super.apply(this, arguments);
   },
