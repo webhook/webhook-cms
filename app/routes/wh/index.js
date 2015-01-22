56e7a91f33fdae476636b86ad15e3e5d215b4f24
Mike Horn <gpbmike@gmail.com>
2015-01-22 14:10:25 -0800

    current message is default.

--- app/routes/wh/index.js
+++ app/routes/wh/index.js
@@ -1,13 +1,32 @@
 export default Ember.Route.extend({
 
+  defaultMessage: function () {
+    var msg = '';
+
+    msg += '#' + Em.I18n.t('wh.index.welcome.title') + "\n\n";
+    msg += Em.I18n.t('wh.index.welcome.description') + "\n\n";
+
+    msg += '* [%@](%@)'.fmt(Em.I18n.t('wh.index.welcome.docs'), "http://www.webhook.com/docs/") + "\n";
+    msg += '* [%@](%@)'.fmt(Em.I18n.t('wh.index.welcome.support'), "http://www.webhook.com/help/") + "\n";
+    msg += '* [%@](%@)'.fmt(Em.I18n.t('wh.index.welcome.issues'), "https://github.com/webhook/webhook/issues?state=open") + "\n";
+    msg += '* [%@](%@)'.fmt(Em.I18n.t('wh.index.welcome.forums'), "http://forums.webhook.com/") + "\n";
+
+    return msg;
+  }.property(),
+
   beforeModel: function () {
     var route = this;
 
     return this.store.find('settings', 'general').then(function (settings) {
+      if (Ember.isEmpty(settings.get('siteMessage'))) {
+        settings.set('siteMessage', route.get('defaultMessage'));
+      }
       route.set('settings', settings);
     }, function (error) {
       var settings = route.store.getById('settings', 'general');
       settings.loadedData();
+
+      settings.set('siteMessage', route.get('defaultMessage'));
       route.set('settings', settings);
     });
 
