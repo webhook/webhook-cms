5594716e8361c1bf23ceb1a10dbe9ec227a1d06b
Mike Horn <gpbmike@gmail.com>
2015-02-02 12:48:59 -0800

    CustomEvent polyfill

--- app/routes/application.js
+++ app/routes/application.js
@@ -115,6 +115,21 @@ export default Ember.Route.extend({
         // Shut down LiveReload
         Ember.Logger.log('ApplicationRoute::getBuildEnvironment::dev::liveReloadCheck');
         if (window.LiveReload && !buildEnv.get('keepReload')) {
+
+          // CustomEvent polyfill
+          if (!window.CustomEvent) {
+            var CustomEvent = function (event, params) {
+              params = params || { bubbles: false, cancelable: false, detail: undefined };
+              var evt = document.createEvent('CustomEvent');
+              evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+              return evt;
+             };
+
+            CustomEvent.prototype = window.Event.prototype;
+
+            window.CustomEvent = CustomEvent;
+          }
+
           var shutDown = new CustomEvent('LiveReloadShutDown');
           document.addEventListener("LiveReloadConnect", function () {
             document.dispatchEvent(shutDown);
