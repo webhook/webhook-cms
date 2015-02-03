0c6b64f7bebeb6edfd2caec40357d671f096409d
Mike Horn <gpbmike@gmail.com>
2015-02-02 20:08:15 -0800

    use modernizr feature detect for CustomEvent

--- app/routes/application.js
+++ app/routes/application.js
@@ -9,60 +9,63 @@ function uniqueId() {
 }
 
 // CustomEvent polyfill
-// try{new window.CustomEvent('?');}catch(o_O){
-//   window.CustomEvent = function(
-//     eventName,
-//     defaultInitDict
-//   ){
-//
-//     // the infamous substitute
-//     function CustomEvent(type, eventInitDict) {
-//       /*jshint eqnull:true */
-//       var event = document.createEvent(eventName);
-//       if (typeof type !== 'string') {
-//         throw new Error('An event name must be provided');
-//       }
-//       if (eventName === 'Event') {
-//         event.initCustomEvent = initCustomEvent;
-//       }
-//       if (eventInitDict == null) {
-//         eventInitDict = defaultInitDict;
-//       }
-//       event.initCustomEvent(
-//         type,
-//         eventInitDict.bubbles,
-//         eventInitDict.cancelable,
-//         eventInitDict.detail
-//       );
-//       return event;
-//     }
-//
-//     // attached at runtime
-//     function initCustomEvent(
-//       type, bubbles, cancelable, detail
-//     ) {
-//       /*jshint validthis:true*/
-//       this.initEvent(type, bubbles, cancelable);
-//       this.detail = detail;
-//     }
-//
-//     // that's it
-//     return CustomEvent;
-//   }(
-//     // is this IE9 or IE10 ?
-//     // where CustomEvent is there
-//     // but not usable as construtor ?
-//     window.CustomEvent ?
-//       // use the CustomEvent interface in such case
-//       'CustomEvent' : 'Event',
-//       // otherwise the common compatible one
-//     {
-//       bubbles: false,
-//       cancelable: false,
-//       detail: null
-//     }
-//   );
-// }
+if (!('CustomEvent' in window && typeof window.CustomEvent === 'function')) {
+
+  Ember.Logger.log('Using CustomEvent polyfill');
+
+  window.CustomEvent = function(
+    eventName,
+    defaultInitDict
+  ){
+
+    // the infamous substitute
+    function CustomEvent(type, eventInitDict) {
+      /*jshint eqnull:true */
+      var event = document.createEvent(eventName);
+      if (typeof type !== 'string') {
+        throw new Error('An event name must be provided');
+      }
+      if (eventName === 'Event') {
+        event.initCustomEvent = initCustomEvent;
+      }
+      if (eventInitDict == null) {
+        eventInitDict = defaultInitDict;
+      }
+      event.initCustomEvent(
+        type,
+        eventInitDict.bubbles,
+        eventInitDict.cancelable,
+        eventInitDict.detail
+      );
+      return event;
+    }
+
+    // attached at runtime
+    function initCustomEvent(
+      type, bubbles, cancelable, detail
+    ) {
+      /*jshint validthis:true*/
+      this.initEvent(type, bubbles, cancelable);
+      this.detail = detail;
+    }
+
+    // that's it
+    return CustomEvent;
+  }(
+    // is this IE9 or IE10 ?
+    // where CustomEvent is there
+    // but not usable as construtor ?
+    window.CustomEvent ?
+      // use the CustomEvent interface in such case
+      'CustomEvent' : 'Event',
+      // otherwise the common compatible one
+    {
+      bubbles: false,
+      cancelable: false,
+      detail: null
+    }
+  );
+}
 
 export default Ember.Route.extend({
   notifications: [],
