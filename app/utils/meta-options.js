950981cfd02eee89b2bdecaf8d97ea66e3188ee4
Mike Horn <gpbmike@gmail.com>
2015-02-16 11:10:38 -0800

    do not let user remove all options.

--- app/utils/meta-options.js
+++ app/utils/meta-options.js
@@ -3,5 +3,8 @@ export default Ember.Object.extend({
     return this.get('options').map(function(option, index) {
       return { option: option, index: index };
     });
-  }.property('options.@each')
+  }.property('options.@each'),
+  isOneOption: function () {
+    return this.get('options.length') === 1;
+  }.property('options.length')
 });
